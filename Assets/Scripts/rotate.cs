a3f6a261bd1aa7fbfe9ce0ca1f1ef743510243eb
KyyevGomes <kyyevgomes27@gmail.com>
2021-02-25 23:35:24 -0600

    Add files

---
+++ Assets/Scripts/rotate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    float speed = 50.0f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+
+    }
+}
